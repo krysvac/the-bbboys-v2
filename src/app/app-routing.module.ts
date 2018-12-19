8febf2e5010f0636d876b8407366af79391e672f
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2018-12-19 14:41:18 +0100

    Formatting fixes and added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {OverviewComponent} from "./overview/overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'overview', component: OverviewComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
