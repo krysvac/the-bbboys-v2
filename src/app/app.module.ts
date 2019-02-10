cf68959542bf85bb9d7096476c6c76c758af62b7
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2019-02-10 03:47:52 +0100

    Add modules needed for things to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {OverviewComponent} from './overview/overview.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { SettingsComponent } from './settings/settings.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LogoutComponent } from './logout/logout.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {LoginComponent} from './login/login.component';
+import {SettingsComponent} from './settings/settings.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {LogoutComponent} from './logout/logout.component';
+import {ApiService, StorageService, UserService} from './_services';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxWebstorageModule} from 'ngx-webstorage';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FoodInfoComponent} from './food-info/food-info.component';
+import {PollComponent} from './poll/poll.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { PollResultsComponent } from './poll-results/poll-results.component';
 
 @NgModule({
     declarations: [
@@ -18,13 +26,21 @@ import { LogoutComponent } from './logout/logout.component';
         LoginComponent,
         SettingsComponent,
         PageNotFoundComponent,
-        LogoutComponent
+        LogoutComponent,
+        FoodInfoComponent,
+        PollComponent,
+        PollResultsComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        NgxWebstorageModule.forRoot(),
+        NgbModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
-    providers: [],
+    providers: [UserService, StorageService, ApiService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
