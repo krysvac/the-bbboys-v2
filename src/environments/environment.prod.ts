cf9285ebbc37075793ff96ce31553dac5da63062
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2019-02-19 20:57:04 +0100

    Updated to correct production api route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     title: ' | The BBBoys mat-info',
     navbarTitle: 'The BBBoys',
-    apiUrl: 'https://thebbboys.se/api'
+    apiUrl: 'https://thebbboys.se/api/public'
 };
